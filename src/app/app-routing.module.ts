daf113c575b6449b71ea0e2a0f8c3ce5f17207dc
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2022-08-30 15:45:19 +0200

    new loginPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -35,7 +35,11 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/pages/home-page/home-page.module.ts
+++ src/app/pages/home-page/home-page.module.ts
@@ -1,12 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './home-page.component';
+import { HeaderModule } from 'src/app/components/header/header.module';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+    // children: [
+    //   {
+    //     path: 'negotiations',
+    //     loadChildren: () => import('../negotiations-page/negotiations-page.module').then(m => m.NegotiationsPageModule)
+    //   },
+    // ]
+  }
+]
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes),
+    HeaderModule
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class HomePageModule { }
