b3f4c454b551bb8e9732ca319ffe276a7e03c1dd
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2022-09-05 16:01:23 +0200

    daily reward

--- src/app/components/profile-right-panel/profile-right-panel.module.ts
+++ src/app/components/profile-right-panel/profile-right-panel.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProfileRightPanelComponent } from './profile-right-panel.component';
-
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
 
 
 @NgModule({
@@ -9,7 +11,10 @@ import { ProfileRightPanelComponent } from './profile-right-panel.component';
     ProfileRightPanelComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatProgressBarModule,
+    MatCardModule,
+    MatIconModule,
   ],
   exports: [
     ProfileRightPanelComponent
