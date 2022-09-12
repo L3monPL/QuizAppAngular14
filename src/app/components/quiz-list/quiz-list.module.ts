d53c38799f60c46465ab086a8fd6610ad8a29cfe
Mikołaj Andrzejewski <mikolaj.andrzejewski999@gmail.com>
2022-09-12 23:34:55 +0200

    quiz list modify

--- src/app/components/quiz-list/quiz-list.module.ts
+++ src/app/components/quiz-list/quiz-list.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuizListComponent } from './quiz-list.component';
 import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatButtonModule} from '@angular/material/button';
 
 
 @NgModule({
@@ -11,6 +14,9 @@ import {MatCardModule} from '@angular/material/card';
   imports: [
     CommonModule,
     MatCardModule,
+    MatIconModule,
+    MatProgressBarModule,
+    MatButtonModule,
   ],
   exports: [
     QuizListComponent
