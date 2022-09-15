dfa9db8a65a98b7a76cbb6af508fd82e7374c943
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2022-09-15 15:39:57 +0200

    icon update

--- src/app/components/quiz-list/quiz-list.component.ts
+++ src/app/components/quiz-list/quiz-list.component.ts
@@ -7,6 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuizListComponent implements OnInit {
 
+  quizList = [
+    {
+      name: 'JavaScript',
+      icon: '../../../assets/js.png',
+      type: 'Front-end',
+      progresValue: 75,
+    },
+    {
+      name: 'CSS',
+      icon: '../../../assets/css.png',
+      type: 'Front-end',
+      progresValue: 55,
+    },
+    {
+      name: 'HTML',
+      icon: '../../../assets/html.png',
+      type: 'Front-end',
+      progresValue: 85,
+    },
+    {
+      name: 'C#',
+      icon: '../../../assets/csharp.png',
+      type: 'Back-end',
+      progresValue: 95,
+    },
+    {
+      name: 'SQL',
+      icon: '../../../assets/js.png',
+      type: 'Back-end',
+      progresValue: 25,
+    },
+    {
+      name: 'C++',
+      icon: '../../../assets/js.png',
+      type: 'Back-end',
+      progresValue: 45,
+    },
+    {
+      name: 'Python',
+      icon: '../../../assets/js.png',
+      type: 'Back-end',
+      progresValue: 5,
+    },
+
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
