7c5e6ad93865d531cd977ab9e3ff465c5e9fb90a
Mikołaj Andrzejewski <mikolaj.andrzejewski999@gmail.com>
2022-09-19 23:46:54 +0200

    quiz map object

--- src/app/components/quiz-map-list/quiz-map-list.component.ts
+++ src/app/components/quiz-map-list/quiz-map-list.component.ts
@@ -10,61 +10,180 @@ export class QuizMapListComponent implements OnInit {
   quizMap = [
     {
       name: 'Angular Developer',
-      
+      progress: 35,
+      quizList: [ //quizy zaciągnięte z userProgress
+        {
+          name: 'HTML',
+          icon: '../../../assets/html.png',
+          type: 'Front-end',
+          progresValue: 85,
+          link: '/home/quiz/3',
+          lesson: [
+            {
+              name: 'Kontenery',
+              description: 'Wszystko o divach w HTML',
+              questions: [
+                {
+                  id: 1,
+                  lessonLevel: 1,
+                  questionConten: "2 + 2 =",
+                  imageUrl: "",
+                  a: "1",
+                  b: "4",
+                  c: "7",
+                  d: "null",
+                  correctAnswer: "B",
+                  lessonId: 1
+                },
+                {
+                  id: 2,
+                  lessonLevel: 1,
+                  questionConten: "4 + 4 =",
+                  imageUrl: "",
+                  a: "1",
+                  b: "4",
+                  c: "8",
+                  d: "null",
+                  correctAnswer: "C",
+                  lessonId: 1
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: 'CSS',
+          icon: '../../../assets/css.png',
+          type: 'Front-end',
+          progresValue: 55,
+          link: '/home/quiz/2',
+          lesson: [
+            {
+              name: 'Kontenery',
+              description: 'Wszystko o divach w HTML',
+              questions: [
+                {
+                  id: 1,
+                  lessonLevel: 1,
+                  questionConten: "2 + 2 =",
+                  imageUrl: "",
+                  a: "1",
+                  b: "4",
+                  c: "7",
+                  d: "null",
+                  correctAnswer: "B",
+                  lessonId: 1
+                },
+                {
+                  id: 2,
+                  lessonLevel: 1,
+                  questionConten: "4 + 4 =",
+                  imageUrl: "",
+                  a: "1",
+                  b: "4",
+                  c: "8",
+                  d: "null",
+                  correctAnswer: "C",
+                  lessonId: 1
+                }
+              ]
+            }
+          ]
+        },
+        {
+          name: 'JavaScript',
+          icon: '../../../assets/js.png',
+          type: 'Front-end',
+          progresValue: 75,
+          link: '/home/quiz/1',
+          lesson: [
+            {
+              name: 'Kontenery',
+              description: 'Wszystko o divach w HTML',
+              questions: [
+                {
+                  id: 1,
+                  lessonLevel: 1,
+                  questionConten: "2 + 2 =",
+                  imageUrl: "",
+                  a: "1",
+                  b: "4",
+                  c: "7",
+                  d: "null",
+                  correctAnswer: "B",
+                  lessonId: 1
+                },
+                {
+                  id: 2,
+                  lessonLevel: 1,
+                  questionConten: "4 + 4 =",
+                  imageUrl: "",
+                  a: "1",
+                  b: "4",
+                  c: "8",
+                  d: "null",
+                  correctAnswer: "C",
+                  lessonId: 1
+                }
+              ]
+            }
+          ]
+        },
+      ]
     }
   ]
 
-  quizList = [
-    {
-      name: 'JavaScript',
-      icon: '../../../assets/js.png',
-      type: 'Front-end',
-      progresValue: 75,
-      link: '/home/quiz/1'
-    },
-    {
-      name: 'CSS',
-      icon: '../../../assets/css.png',
-      type: 'Front-end',
-      progresValue: 55,
-      link: '/home/quiz/2'
-    },
-    {
-      name: 'HTML',
-      icon: '../../../assets/html.png',
-      type: 'Front-end',
-      progresValue: 85,
-      link: '/home/quiz/3'
-    },
-    {
-      name: 'C#',
-      icon: '../../../assets/csharp.png',
-      type: 'Back-end',
-      progresValue: 95,
-      link: '/home/quiz/4'
-    },
-    {
-      name: 'SQL',
-      icon: '../../../assets/sql.png',
-      type: 'Back-end',
-      progresValue: 25,
-      link: '/home/quiz/5'
-    },
-    {
-      name: 'C++',
-      icon: '../../../assets/c++.png',
-      type: 'Back-end',
-      progresValue: 45,
-      link: '/home/quiz/6'
-    },
-    {
-      name: 'Python',
-      icon: '../../../assets/python.png',
-      type: 'Back-end',
-      progresValue: 5,
-      link: '/home/quiz/7'
-    },
-  ]
+  // quizList = [
+  //   {
+  //     name: 'JavaScript',
+  //     icon: '../../../assets/js.png',
+  //     type: 'Front-end',
+  //     progresValue: 75,
+  //     link: '/home/quiz/1'
+  //   },
+  //   {
+  //     name: 'CSS',
+  //     icon: '../../../assets/css.png',
+  //     type: 'Front-end',
+  //     progresValue: 55,
+  //     link: '/home/quiz/2'
+  //   },
+  //   {
+  //     name: 'HTML',
+  //     icon: '../../../assets/html.png',
+  //     type: 'Front-end',
+  //     progresValue: 85,
+  //     link: '/home/quiz/3'
+  //   },
+  //   {
+  //     name: 'C#',
+  //     icon: '../../../assets/csharp.png',
+  //     type: 'Back-end',
+  //     progresValue: 95,
+  //     link: '/home/quiz/4'
+  //   },
+  //   {
+  //     name: 'SQL',
+  //     icon: '../../../assets/sql.png',
+  //     type: 'Back-end',
+  //     progresValue: 25,
+  //     link: '/home/quiz/5'
+  //   },
+  //   {
+  //     name: 'C++',
+  //     icon: '../../../assets/c++.png',
+  //     type: 'Back-end',
+  //     progresValue: 45,
+  //     link: '/home/quiz/6'
+  //   },
+  //   {
+  //     name: 'Python',
+  //     icon: '../../../assets/python.png',
+  //     type: 'Back-end',
+  //     progresValue: 5,
+  //     link: '/home/quiz/7'
+  //   },
+  // ]
 
   constructor() { }
 
--- src/app/components/quiz-map-list/quiz-map-list.module.ts
+++ src/app/components/quiz-map-list/quiz-map-list.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuizMapListComponent } from './quiz-map-list.component';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -9,7 +10,8 @@ import { QuizMapListComponent } from './quiz-map-list.component';
     QuizMapListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [
     QuizMapListComponent
