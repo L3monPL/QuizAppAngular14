3930b65d1c4236efe6b33ef62c9f5c410bab5aa1
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2022-09-23 14:38:09 +0200

    quiz info visual update

--- src/app/components/information-about-quiz-map/information-about-quiz-map.component.ts
+++ src/app/components/information-about-quiz-map/information-about-quiz-map.component.ts
@@ -8,11 +8,23 @@ import { QuizMapServiceService } from 'src/app/services/quiz-map-service.service
 })
 export class InformationAboutQuizMapComponent implements OnInit {
 
+  lessonsPanel = false
+
   constructor(
     public quizMapService: QuizMapServiceService
   ) { }
 
   ngOnInit(): void {
+  }
+
+  showLessonPanel(){
+    if (this.lessonsPanel == false) {
+      this.lessonsPanel = true
+    }
+    else if (this.lessonsPanel == true) {
+      this.lessonsPanel = false
+    }
+    // this.lessonsPanel = true
   }
 
 }
