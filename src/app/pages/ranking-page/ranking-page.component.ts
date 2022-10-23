d3891f3120caa2b971ce171b2788bf13ba3ffa40
Mikołaj Andrzejewski <mikolaj.andrzejewski999@gmail.com>
2022-10-23 19:38:27 +0200

    add json list to rankings

--- src/app/pages/ranking-page/ranking-page.component.ts
+++ src/app/pages/ranking-page/ranking-page.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RankingPageComponent implements OnInit {
 
+  ranking = [
+    {
+      name: 'Mikołaj',
+      points: 999
+    },
+    {
+      name: 'Franek',
+      points: 879
+    },
+    {
+      name: 'Teodor',
+      points: 700
+    },
+    {
+      name: 'Kajetan',
+      points: 546
+    },
+    {
+      name: 'Witold',
+      points: 232
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
