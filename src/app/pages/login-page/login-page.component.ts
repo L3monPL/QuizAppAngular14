e891904003200dd4aecd9e2a2334b31009002075
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2022-10-27 15:42:21 +0200

    init desc token

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Component({
   selector: 'app-login-page',
@@ -8,6 +9,8 @@ import { Router } from '@angular/router';
 })
 export class LoginPageComponent implements OnInit {
 
+  helper = new JwtHelperService();
+
   constructor(private router: Router) { }
 
   ngOnInit(): void {
@@ -15,6 +18,31 @@ export class LoginPageComponent implements OnInit {
     {
       this.router.navigate(['./login'])
     }
+
+    this.autoLogin()
+  }
+  autoLogin(){
+    let urlId = "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"
+    let token = this.helper.decodeToken(localStorage.getItem('currentUser')!);
+    console.log(token)
+    let elo = urlId.valueOf()
+    console.log(urlId.valueOf())
+    console.log(token)
+    const myJSON = JSON.stringify(token);
+    // console.log(myJSON[3])
+
+
+    // const crypto = require('crypto');
+    // const fs = require('fs'); 
+    // const sign = crypto.createSign('RSA-SHA256');
+
+    // // copy / paste here the header and the payload of your JWT only
+    // sign.write('eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9');
+    // sign.end();
+
+    // var privateKey = fs.readFileSync('./private.key');
+
+    // console.log(sign.sign(privateKey, 'base64'));
   }
 
 }
