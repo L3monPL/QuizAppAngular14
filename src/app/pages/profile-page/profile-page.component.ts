9d15a90fafbd8934d6f625efdca30f0c05ab79cd
Mikołaj Andrzejewski <mikolaj.andrzejewski999@gmail.com>
2022-11-09 22:24:07 +0100

    change profilePage

--- src/app/pages/profile-page/profile-page.component.ts
+++ src/app/pages/profile-page/profile-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDataService } from 'src/app/services/global-services/user-data.service';
 
 @Component({
   selector: 'app-profile-page',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public userData: UserDataService
+  ) { }
 
   ngOnInit(): void {
   }
