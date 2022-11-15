a2608da2db0f8383097c865c0e7f2d7d12e6e912
mikolaj andrzejewski <mikolaj.andrzejewski@daw-systems.pl>
2022-11-15 14:16:48 +0100

    init imageSwap

--- src/app/components/USER-PROFILE-PAGE/edit-profil-profile/edit-profil-profile.component.ts
+++ src/app/components/USER-PROFILE-PAGE/edit-profil-profile/edit-profil-profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserDataService } from 'src/app/services/global-services/user-data.service';
+
+
 
 @Component({
   selector: 'app-edit-profil-profile',
@@ -7,9 +10,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditProfilProfileComponent implements OnInit {
 
-  constructor() { }
+  images =[
+    {
+      profileImg: "https://wsblearnstorage.blob.core.windows.net/imagecontainer/User_Icon_0"
+    },
+    {
+      profileImg: "https://wsblearnstorage.blob.core.windows.net/imagecontainer/Quiz Form"
+    },
+    {
+      profileImg: "https://wsblearnstorage.blob.core.windows.net/imagecontainer/WSB XD"
+    },
+    // {
+    //   profileImg: "https://wsblearnstorage.blob.core.windows.net/imagecontainer/User_Icon_0"
+    // },
+    // {
+    //   profileImg: "https://wsblearnstorage.blob.core.windows.net/imagecontainer/User_Icon_0"
+    // },
+    // {
+    //   profileImg: "https://wsblearnstorage.blob.core.windows.net/imagecontainer/User_Icon_0"
+    // },
+    // {
+    //   profileImg: "https://wsblearnstorage.blob.core.windows.net/imagecontainer/User_Icon_0"
+    // },
+  ]
+
+  currentProfileImage?: string
+
+  selectedItem?: any
+
+
+  constructor(
+    public userData: UserDataService,
+  ) { }
 
   ngOnInit(): void {
+    // console.log(this.userData.getProfileIcon())
+    this.checkCurrentImage()
+  }
+
+  checkCurrentImage(){
+    for (let index = 0; index < this.images.length; index++) {
+      if (this.images[index].profileImg == this.userData.getProfileIcon()) {
+        this.currentProfileImage = this.images[index].profileImg
+        console.log(this.currentProfileImage)
+        this.selectedItem = this.currentProfileImage
+      }
+    }
+
+  }
+
+  onClickSelect(img: any){
+    this.selectedItem = img.profileImg;
+    console.log(this.selectedItem)
+    // this.showLessonsFromQuizMap = null
+    // this.lastId = null
   }
 
 }
