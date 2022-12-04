27758e2e0a0c5632a64c243a77085e3a4c883d54
Mikołaj Andrzejewski <mikolaj.andrzejewski999@gmail.com>
2022-12-04 15:48:42 +0100

    change

--- src/app/components/ADMIN-PANEL-PAGE/edit-question/edit-question.component.ts
+++ src/app/components/ADMIN-PANEL-PAGE/edit-question/edit-question.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { CategoryManagerRestService } from 'src/app/services/components-services/category-manager-rest.service';
+import { QuestionRestService } from 'src/app/services/question-rest.service';
 
 @Component({
   selector: 'app-edit-question',
@@ -7,9 +11,102 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditQuestionComponent implements OnInit {
 
-  constructor() { }
+  categoryList?: any
+  subQuestions?: Subscription
+  customErrorCategoryId?: string
+
+  categoryEditForm = new FormGroup({
+    categoryId: new FormControl<number|null>(null,Validators.required),
+  });
+
+  constructor(
+    public categoryManagerService: CategoryManagerRestService,
+    public questionRestSerivice: QuestionRestService
+  ) { }
 
   ngOnInit(): void {
+    this.subscribeCategoryList()
   }
 
+  editCategorySubmit(){
+    let categoryId = this.categoryEditForm.get('categoryId')?.value
+   
+    if (categoryId != null) {
+      if (this.categoryEditForm.valid) {
+        this.getQUestionsList(categoryId!)
+      // this.getQuestionsValueToFOrm(categoryId!) to startuje przy wyborze pytania z listy pytań
+      }
+    }
+    
+  }
+
+  subscribeCategoryList(){
+    this.categoryManagerService.serviceCategory.subscribe(
+      res => {
+        this.categoryList = res
+        console.log(this.categoryList)
+      },
+      error => {}, 
+      () => {})
+
+  }
+
+  getQUestionsList(id: number){
+    this.subQuestions = this.questionRestSerivice.getQuestionsListByCategoryIdAndLvl(id, 1).subscribe({
+        next: (response) => {
+          if (response.body) {
+            // this.categoryById = response.body
+            // this.addCategoryForm.controls['name'].setValue(this.categoryById.name)
+            // this.addCategoryForm.controls['description'].setValue(this.categoryById.description!)
+            // this.addCategoryForm.controls['iconUrl'].setValue(this.categoryById.iconUrl!)
+            // this.addCategoryForm.controls['questionsPerLesson'].setValue(this.categoryById.questionsPerLesson)
+            // this.addCategoryForm.controls['lessonsPerLevel'].setValue(this.categoryById.lessonsPerLevel)
+            // this.showValuesToEditCategory = true
+            // this.loadingEditingQuiz = false
+          }
+          else{
+            this.customErrorCategoryId = 'Brak obiektu odpowiedzi'
+            // this.loadingEditingQuiz = false
+          } 
+        },
+        error: (errorResponse) => {
+              this.customErrorCategoryId = errorResponse.error;
+              // this.loadingEditingQuiz = false
+        },
+        complete: () => {}
+      })
+    console.log('tutaj pokazujemy listę pytań')
+  }
+
+  getQuestionsValueToFOrm(id: number){
+    // this.loadingEditingQuiz = true
+    // this.subQuestions = this.questionRestSerivice.getQuestionsListByCategoryIdAndLvl(id).subscribe({
+    //   next: (response) => {
+    //     if (response.body) {
+    //       this.categoryById = response.body
+    //       this.addCategoryForm.controls['name'].setValue(this.categoryById.name)
+    //       this.addCategoryForm.controls['description'].setValue(this.categoryById.description!)
+    //       this.addCategoryForm.controls['iconUrl'].setValue(this.categoryById.iconUrl!)
+    //       this.addCategoryForm.controls['questionsPerLesson'].setValue(this.categoryById.questionsPerLesson)
+    //       this.addCategoryForm.controls['lessonsPerLevel'].setValue(this.categoryById.lessonsPerLevel)
+    //       this.showValuesToEditCategory = true
+    //       this.loadingEditingQuiz = false
+    //     }
+    //     else{
+    //       this.customErrorCategoryId = 'Brak obiektu odpowiedzi'
+    //       this.loadingEditingQuiz = false
+    //     } 
+    //   },
+    //   error: (errorResponse) => {
+    //         this.customErrorCategoryId = errorResponse.error;
+    //         this.loadingEditingQuiz = false
+    //   },
+    //   complete: () => {}
+    // })
+    console.log('tutaj inicjujemy wartości pytań do formularze')
+  }
+
+  get f(){
+    return this.categoryEditForm.controls;
+  }
 }
--- src/app/components/ADMIN-PANEL-PAGE/edit-question/edit-question.module.ts
+++ src/app/components/ADMIN-PANEL-PAGE/edit-question/edit-question.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EditQuestionComponent } from './edit-question.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 
 
 
@@ -9,7 +15,13 @@ import { EditQuestionComponent } from './edit-question.component';
     EditQuestionComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSelectModule,
+    MatProgressSpinnerModule
   ],
   exports: [
     EditQuestionComponent
